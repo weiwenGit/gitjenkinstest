252d3d1e22eda5836f505b63c41df25ad845074d
weiwen <6829512@qq.com>
2018-04-30 11:39:07 +0800

    新建

---
+++ gitjenkins/src/main/java/com/bibispace/gitjenkins/GitjenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.bibispace.gitjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitjenkinsApplication.class, args);
+	}
+}
---
+++ gitjenkins/src/test/java/com/bibispace/gitjenkins/GitjenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bibispace.gitjenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitjenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
